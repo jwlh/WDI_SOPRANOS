c82ab6b70a9dfebe697c7d46420d61dd157622d6
jwlh <jonnyhall@me.com>
2017-11-24 14:01:13 +0000

    setting up basic front-end

---
+++ front-end/src/js/app.js
@@ -0,0 +1,16 @@
+// app.js is the main JS file which you should define your Angular module
+angular
+  .module('sopranos', [])
+  .controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$http'];
+
+function MainCtrl($http) {
+  const vm = this;
+
+  $http
+    .get('http://localhost:3000/questions')
+    .then(res => {
+      vm.questions = res.data;
+    });
+}
